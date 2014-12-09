f9fd96368d6586ef9c2b2caef0013189bec4699a
spoliv <spoliv@rambler.ru>
2014-12-09 23:04:31 +0300

    Commit 10.12

--- models/news.php
+++ models/news.php
@@ -8,10 +8,20 @@ function News_getAll()
     SELECT * FROM news
     ");
 }
-if (isset($_POST['article']) && isset($_POST['title']))
-{
-    $artar = $_POST['article'];
-    $artti = $_POST['title'];
-    DBConnect();
-    mysql_query("INSERT INTO `news` (`title`, `text`) VALUES('$artti', '$artar')");
-}
+    if (isset($_POST['article']) && isset($_POST['title']))
+    {
+        $artar = $_POST['article'];
+        $artti = $_POST['title'];
+        DBConnect();
+        mysql_query("INSERT INTO news (title, text) VALUES('$artti', '$artar')");
+    }
+
+    if(isset($_GET['id']))
+    {
+        $artic = $_GET['id'];
+        DBConnect();
+        $res=mysql_query("SELECT * FROM news WHERE id=$artic");
+        $row=mysql_fetch_array($res);
+
+            echo $row['title'], $row['text'];
+    }
