4f121b811fd1dd45be1c722a1a71a193b4700914
spoliv <spoliv@rambler.ru>
2014-12-09 11:57:07 +0300

    Commit 09.12

--- index.php
+++ index.php
@@ -5,3 +5,4 @@ require_once __DIR__ . '/models/news.php';
 $news = News_getAll();
 
 include 'view/index.php';
+include 'view/form.php';
--- view/form.php
+++ view/form.php
@@ -1,7 +1,15 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1
- * Date: 09.12.2014
- * Time: 3:27
- */ 
+<html>
+<head>
+    <title>Добавить новость</title>
+</head>
+<body>
+<br />
+<br />
+<form action="functions.php" method="post">
+    <legend><h3>Добавить новость</h3></legend>
+    <textarea cols="30" rows="5" name="title"></textarea>
+    <br/>
+    <input type="submit" value="добавить" />
+</form>
+</body>
+</html>
